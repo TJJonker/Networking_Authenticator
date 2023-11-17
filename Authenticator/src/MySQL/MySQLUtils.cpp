53fc1f8b35e14de542b17bc60082dce176d769d1
Tom Jonker <tjj.jonker@gmail.com>
2023-11-16 21:50:35 -0500

    Small addition

--- Authenticator/src/MySQL/MySQLUtils.cpp
+++ Authenticator/src/MySQL/MySQLUtils.cpp
@@ -1,1 +1,22 @@
 #include "MySQLUtils.h"
+
+MySQLUtils::MySQLUtils()
+{
+}
+
+MySQLUtils::~MySQLUtils()
+{
+	Disconnect();
+}
+
+void MySQLUtils::ConnectToDatabase(const char* host, const char* username, const char* password)
+{
+	m_Driver = sql::mysql::get_mysql_driver_instance();
+	m_Connection = m_Driver->connect(host, username, password);
+}
+
+void MySQLUtils::Disconnect()
+{
+	if(m_Connection)
+		delete m_Connection;
+}
--- Authenticator/src/MySQL/MySQLUtils.h
+++ Authenticator/src/MySQL/MySQLUtils.h
@@ -1,7 +1,17 @@
 #pragma once
+#include <MySQL/jdbc.h>
 
 class MySQLUtils
 {
+public:
+	MySQLUtils();
+	~MySQLUtils();
 
+	void ConnectToDatabase(const char* host, const char* username, const char* password);
+	void Disconnect();
+
+private:
+	sql::mysql::MySQL_Driver* m_Driver;
+	sql::Connection* m_Connection;
 };
 
