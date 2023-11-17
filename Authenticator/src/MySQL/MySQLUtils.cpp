37e8acc9b13c20597e96dd519de86b8b9463580b
Tom Jonker <tjj.jonker@gmail.com>
2023-11-16 23:08:02 -0500

    Update MySQLUtils.cpp

--- Authenticator/src/MySQL/MySQLUtils.cpp
+++ Authenticator/src/MySQL/MySQLUtils.cpp
@@ -17,16 +17,17 @@ void MySQLUtils::ConnectToDatabase(const char* host, const char* username, const
 	try {
 		m_Driver = sql::mysql::get_mysql_driver_instance();
 		m_Connection = m_Driver->connect(host, username, password);
+		TWONET_CORE_TRACE("Succesfully connected to the database.")
 	}
 	catch (sql::SQLException& e) {
-		TWONET_ERROR("Could not establish connection with the database: {0}", e.what());
+		TWONET_CORE_ERROR("Could not establish connection with the database: {0}", e.what());
 		return;
 	}
 }
 
 void MySQLUtils::Disconnect()
 {
-	TWONET_ASSERT(m_Connection, "No connection to disconnect.");
+	TWONET_CORE_ASSERT(m_Connection, "No connection to disconnect.");
 	delete m_Connection;
 }
 
